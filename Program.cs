af9ef7b593ee14738f07e10171592278ef0aec4a
davidgm05 <dapnestic05@gmail.com>
2023-04-14 08:43:03 +0200

    done

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine()
+Console.WriteLine("Hola,¿cómo te llamas?");
 string name;
 name=Console.ReadLine();
-Console.WriteLine("Hola," + name +"!");
+Console.WriteLine("¡Hola," + name +"!");
